9233cbc095ccf9e571ed6d625f9f9a4d07543219
Jan Jongboom <janjongboom@gmail.com>
2012-04-20 15:05:55 +0200

    OSX zip util

--- zip.js
+++ zip.js
@@ -1,7 +1,17 @@
+var Path = require("path");
 var exec = require("child_process").exec;
 
 module.exports.zipUpAFolder = function (zipFile, directory, callback) {
-    var command = 'zip -r -0 -q "' + zipFile + '" "' + directory + '"';
+    zipFile = Path.resolve(zipFile);
+    directory = Path.resolve(directory);
+    
+    var commands = [
+        'cd "' + directory + '"',
+        'zip -r -0 -q "' + zipFile + '" .'
+    ];
+    
+    var command = commands.join("; ");
+    //console.log("cmd", command);
     
     exec(command, function (err, stdout, stderr) {
         if (err || stderr) return callback(err || stderr);
